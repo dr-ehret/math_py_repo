839e2a0bd39d65c0b9104a8c1cedaab6055dacf2
varreli <varreli@gmail.com>
2015-08-23 22:59:53 -0700

    First sympy yay.

---
+++ first_sympy.py
@@ -0,0 +1,30 @@
+import numpy, sympy
+
+from sympy import symbols, expand, factor
+
+print(sympy.sqrt(16))
+
+print(numpy.sqrt(3)) # => 1.73205080757
+print(sympy.sqrt(3))
+
+print(numpy.sqrt(8))
+print(sympy.sqrt(8)) # => simplifies symbolically: 2*sqrt(2)
+
+
+##
+
+x, y = symbols('x y')
+expr = x + 2*y
+print(expr)
+
+print(expr + 1)
+print(expr - x)
+
+new_expr = expr * x
+expanded_expr = (expand(new_expr))
+print("%s : the expression is expanded." %expanded_expr)
+
+factored_expr = (factor(expanded_expr))
+print("%s : the expansion reverts back to factored form." %factored_expr)
+
+ 
---
+++ sympy_compute.py
@@ -0,0 +1,11 @@
+import sympy
+import numpy
+
+print(sympy.sqrt(16))
+
+print(numpy.sqrt(3)) # => 1.73205080757
+print(sympy.sqrt(3))
+
+print(numpy.sqrt(8))
+print(sympy.sqrt(8)) # => simplifies symbolically: 2*sqrt(2)
+ 
